b1019c4de1b12b0fea30edc2c42be434e8d0e0c0
eloirotava <46318465+eloirotava@users.noreply.github.com>
2026-03-20 10:37:40 +0000

    ident

--- main.py
+++ main.py
@@ -442,6 +442,6 @@ async def nuke_vps(id_vps: str, admin: str = Depends(verificar_admin)):
     conn.close()
     return RedirectResponse(url="/ops?msg=Nuked", status_code=303)
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
